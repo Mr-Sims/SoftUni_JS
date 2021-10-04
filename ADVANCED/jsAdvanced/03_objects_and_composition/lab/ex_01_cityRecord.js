8b29322fac19467b8c078ff7d1fedf55cf3ec36b
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2021-10-04 23:28:23 +0300

    school problems and exercises

---
+++ ADVANCED/jsAdvanced/03_objects_and_composition/lab/ex_01_cityRecord.js
@@ -0,0 +1,12 @@
+function cityRecord(name, population, treasury) {
+    const result = {
+        name: name,
+        population: population,
+        treasury: treasury
+    };
+    return result
+}
+
+cityRecord('Tortuga',7000,15000);
+
+cityRecord('Santo Domingo',12000,23500);
